991b05e2238312a904d02e3de0f9d569b3558ce6
Aleksey <kharlampiev@aspect-systems.ru>
2019-09-20 14:03:39 +0300

    * Реализовал всю основную часть логики * Добавил комментарии по каждой функции для понимания хода моих мыслей

--- js/main.js
+++ js/main.js
@@ -8,13 +8,15 @@ var CHECKOUT = ['12:00', '13:00', '14:00'];
 var FEATURES = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
 var DESCRIIPTIONS = ['есть парковка рядом с домом', 'можно с животными но без детей', 'можно с детьми но без животных', 'можно только одним животным или только одним детям', 'воды нет но рядом есть магазин'];
 var PHOTOS = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
-
+//функция получения рандомного элемента из массива
 var getRandomElement = function (arr) {
     return arr[Math.floor(Math.random() * (0 - arr.length)) + arr.length];
 };
+//функция получения рандомгного числа в заданном интервале
 var getRandomNumber = function(from, to) {
     return Math.floor(Math.random() * (to - from) + from);
 };
+//функция генерации рандомного массива из заданного массива
 var getRandomArray = function(arr) {
     var randomArr = [];
     for (var i = 0; i < getRandomNumber(1, arr.length); i++) {
@@ -22,7 +24,7 @@ var getRandomArray = function(arr) {
     }
     return randomArr;
 };
-
+//генерация шаблонного объекта с данными
 var makeSimilarAd = function () {
     var ad = {
         'author': {
@@ -48,7 +50,7 @@ var makeSimilarAd = function () {
     };
     return ad;
 };
-
+//генерация массива с объктами данных
 var generateAds = function (count) {
     var ads = [];
     for (var i = 0; i < count; i++) {
@@ -57,3 +59,35 @@ var generateAds = function (count) {
     return ads;
 
 };
+
+var map = document.querySelector('.map');
+var mapPins = document.querySelector('.map__pins');
+var template_pin = document.querySelector('#pin').content;
+var pin = template_pin.querySelector('.map__pin');
+
+//открываем карту...
+map.classList.remove('map--faded');
+
+//функция-шаблон для создания метки
+var getDrawPin = function (element) {
+    var clonePin = pin.cloneNode(true);
+    var img = clonePin.querySelector('img');
+    clonePin.style.left = element.location.x + 'px';
+    clonePin.style.top = element.location.y + 'px';
+    img.src = element.author.avatar;
+    img.alt = element.offer.title;
+    return clonePin;
+};
+//функция создания меток на основе массива с данными
+var getSimilarAds = function (dataArray) {
+    var fragment = new DocumentFragment;
+    for (var i = 0; i < dataArray.length; i++) {
+        fragment.prepend(getDrawPin(dataArray[i]));
+    }
+    return fragment
+};
+//отрисовка меток на карте
+var render = function (element, pasteElements) {
+    element.prepend(pasteElements);
+}
+render(mapPins, getSimilarAds(generateAds(8)));
