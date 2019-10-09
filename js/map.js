d6632fe320039484e1f868ba6c67c98c31705582
Aleksey <kharlampiev@aspect-systems.ru>
2019-10-09 17:34:26 +0300

    Перетаскивание метки

--- js/map.js
+++ js/map.js
@@ -1,17 +1,56 @@
 'use strict';
 (function () {
-  var dom = {};
-  dom.map = document.querySelector('.map');
-  dom.form = document.querySelector('.ad-form');
-  dom.fieldset = dom.form.querySelectorAll('fieldset');
-  dom.formAddress = dom.form.querySelector('input[name="address"]');
-  dom.mainPin = dom.map.querySelector('.map__pin--main');
+  var Dom = {};
+  Dom.map = document.querySelector('.map');
+  Dom.form = document.querySelector('.ad-form');
+  Dom.fieldset = Dom.form.querySelectorAll('fieldset');
+  Dom.formAddress = Dom.form.querySelector('input[name="address"]');
+  Dom.mainPin = Dom.map.querySelector('.map__pin--main');
 
   var makeIsActivate = function (collection) {
     for (var i = 0; i < collection.length; i++) {
       collection[i].removeAttribute('disabled');
     }
   };
+  var mainPinMousedownHandler = function (evt) {
+    evt.preventDefault();
+    Dom.map.classList.remove('map--faded');
+    Dom.form.classList.remove('ad-form--disabled');
+    markLocation.installPinAddress(Dom.mainPin, true);
+    makeIsActivate(Dom.fieldset);
+    var initialCoord = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+    var maimPinMouseMovehandler = function (moveEvt) {
+      moveEvt.preventDefault();
+      var offset = {
+        x: initialCoord.x - moveEvt.clientX,
+        y: initialCoord.y - moveEvt.clientY
+      };
+      initialCoord = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+      markLocation.installPinAddress(Dom.mainPin, true);
+      Dom.mainPin.style.top = (Dom.mainPin.offsetTop - offset.y) + 'px';
+      Dom.mainPin.style.left = (Dom.mainPin.offsetLeft - offset.x) + 'px';
+    };
+    var maimPinMouseUphandler = function (upEvt) {
+      upEvt.preventDefault();
+      document.removeEventListener('mousemove', maimPinMouseMovehandler);
+      document.removeEventListener('mouseup', maimPinMouseUphandler);
+    };
+    document.addEventListener('mousemove', maimPinMouseMovehandler);
+    document.addEventListener('mouseup', maimPinMouseUphandler);
+
+  };
+  Dom.mainPin.addEventListener('mousedown', mainPinMousedownHandler);
+  Dom.mainPin.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 13) {
+      mainPinMousedownHandler();
+    }
+  });
   var markLocation = {};
   markLocation.getMarkCoord = function (mark, active) {
     var markRect = mark.getBoundingClientRect();
@@ -28,14 +67,14 @@
     return markCoordinate;
   };
   markLocation.getPinHeight = function (mark) { // высота главной метки состоит
-    var img = dom.mainPin.querySelector('img'); // из высоты картинки внутри  метки без отступов +
+    var img = Dom.mainPin.querySelector('img'); // из высоты картинки внутри  метки без отступов +
     var pinAfterStyle = getComputedStyle(mark, '::after'); // высота псевдоэлемента
     var imgHeight = img.clientHeight;
     var afterHeight = parseInt(pinAfterStyle.height, 10);
     var pinHeight = afterHeight + imgHeight;
     return pinHeight;
   };
-  var installPinAddress = function (pinMap, active) {
+  markLocation.installPinAddress = function (pinMap, active) {
     var coordinate = markLocation.getMarkCoord(pinMap, active);
     var y = Math.floor(coordinate.y);
     var x = Math.floor(coordinate.x);
@@ -46,19 +85,8 @@
       left = x + ' расстояние до острого конца по горизонтали';
     }
     var value = left + ', ' + top;
-    dom.formAddress.value = value;
+    Dom.formAddress.value = value;
   };
-  var mainPinMousedownHandler = function () {
-    dom.map.classList.remove('map--faded');
-    dom.form.classList.remove('ad-form--disabled');
-    installPinAddress(dom.mainPin, true);
-    makeIsActivate(dom.fieldset);
-  };
-  installPinAddress(dom.mainPin, false);
-  dom.mainPin.addEventListener('mousedown', mainPinMousedownHandler);
-  dom.mainPin.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === 13) {
-      mainPinMousedownHandler();
-    }
-  });
+  markLocation.installPinAddress(Dom.mainPin, false);
+  window.markLocation = markLocation;
 })();
---
+++ js/pinmove.js
@@ -0,0 +1,6 @@
+'use strict';
+(function () {
+  var Dom = {};
+  Dom.map = document.querySelector('.map');
+  Dom.maiPin = Dom.map.querySelector('.map__pin--main');
+})();
