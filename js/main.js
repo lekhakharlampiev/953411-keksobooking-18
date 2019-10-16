fbb76a0eea7af89e2e3fe8320df3fe852e7156b3
Aleksey <kharlampiev@aspect-systems.ru>
2019-10-16 17:37:47 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 'use strict';
 (function () {
+  var dom = window.domElements;
+  dom.fieldsets.forEach(function (element) {
+    console.log(element.value);
+  });
+  var pageToInactive = function () {
+
+  };
 
 })();
